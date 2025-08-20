a77d9f298d5c828a7d749f0bdfd65e5e821ee324
EtimbukUdofia <etimbukudofia9@gmail.com>
2025-08-20 13:25:52 +0100

    feat(client): refactor routing in App component and wrap with BrowserRouter

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,20 @@
-import React from 'react'
-import LoginPage from './pages/auth/LoginPage'
+import { Route, Routes } from 'react-router'
+import { LoginForm } from './components/forms/auth/LoginForm'
+import { SignupForm } from './components/forms/auth/SignupForm'
+import AuthLayout from './components/layouts/AuthLayout'
 
 function App() {
   return (
-    <div>
-      <LoginPage />
-    </div>
+    <Routes>
+      <Route path="/" element={<div>Home Page</div>} />
+
+      {/* Auth Layout */}
+      <Route element={<AuthLayout />} >
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/signup" element={<SignupForm />} />
+      </Route>
+      
+    </Routes>
   )
 }
 
