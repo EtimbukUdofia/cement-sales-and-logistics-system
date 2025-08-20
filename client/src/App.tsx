3553e26b0f6f55a10a1bf6fa9457f477e6251b9c
EtimbukUdofia <etimbukudofia9@gmail.com>
2025-08-20 13:37:08 +0100

    feat(client): update routing for admin authentication and include NotFound page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,8 @@
 import { Route, Routes } from 'react-router'
-import { LoginForm } from './components/forms/auth/LoginForm'
-import { SignupForm } from './components/forms/auth/SignupForm'
+import { LoginForm } from './components/forms/auth/admin/LoginForm'
+import { SignupForm } from './components/forms/auth/admin/SignupForm'
 import AuthLayout from './components/layouts/AuthLayout'
+import NotFound from './pages/NotFound'
 
 function App() {
   return (
@@ -10,10 +11,12 @@ function App() {
 
       {/* Auth Layout */}
       <Route element={<AuthLayout />} >
-        <Route path="/login" element={<LoginForm />} />
-        <Route path="/signup" element={<SignupForm />} />
+        <Route path="/admin/login" element={<LoginForm />} />
+        <Route path="/admin/signup" element={<SignupForm />} />
       </Route>
       
+      {/* Not Found Page */}
+      <Route path="*" element={<NotFound />} />
     </Routes>
   )
 }
