1fbb24d654e96551c4d4a30d5112be288be42f12
EtimbukUdofia <etimbukudofia9@gmail.com>
2025-08-23 00:22:13 +0100

    feat(client): implement ProtectedRoute and RedirectIfAuthenticated components for enhanced authentication flow

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,54 @@
-import { Route, Routes } from 'react-router'
+import { Navigate, Route, Routes } from 'react-router'
 import { LoginForm } from './components/forms/auth/admin/LoginForm'
 import { SignupForm } from './components/forms/auth/admin/SignupForm'
 import AuthLayout from './components/layouts/AuthLayout'
 import NotFound from './pages/NotFound'
 import SalesLoginForm from './components/forms/auth/salesPerson/SalesLoginForm'
+import { useAuthStore } from './store/authStore'
+import { type JSX } from 'react'
+
+const ProtectedRoute = ({ children }: { children: JSX.Element }) => {
+  const { isAuthenticated } = useAuthStore();
+
+  if (!isAuthenticated) {
+    return <Navigate to="/admin/login" replace />
+  }
+
+  return children;
+}
+
+const RedirectIfAuthenticated = ({ children }: { children: JSX.Element }) => {
+  const { isAuthenticated } = useAuthStore();
+
+  if (isAuthenticated) {
+    return <Navigate to="/" replace />;
+  }
+
+  return children;
+}
 
 function App() {
+  // const { checkAuth } = useAuthStore();
+
+  // Check authentication status on app load
+  // useEffect(() => {
+  //   checkAuth();
+  // }, [checkAuth]);
+  
   return (
     <Routes>
-      <Route path="/" element={<div>Home Page</div>} />
+      <Route path="/" element={
+        <ProtectedRoute>
+          <div>Home Page</div>
+        </ProtectedRoute>
+      } />
 
       {/* Auth Layout */}
-      <Route element={<AuthLayout />} >
+      <Route element={
+        <RedirectIfAuthenticated>
+          <AuthLayout />
+        </RedirectIfAuthenticated>
+      } >
         <Route path="/login" element={<SalesLoginForm />} />
 
         {/* Admin Auth Forms */}
