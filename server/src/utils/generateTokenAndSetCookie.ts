df7c37af734f4a3e31190b47a16bcf800ac423e5
EtimbukUdofia <etimbukudofia9@gmail.com>
2025-08-25 15:56:07 +0100

    feat(server): generate JWT token and save cookies on client

---
+++ server/src/utils/generateTokenAndSetCookie.ts
@@ -0,0 +1,26 @@
+import jwt from "jsonwebtoken";
+import { type Response } from "express";
+import type { Types } from "mongoose";
+import type { StringValue } from "ms";
+
+interface Payload {
+  userId: Types.ObjectId;
+  role: 'admin' | 'salesPerson';
+}
+
+const generateTokenAndSetCookie = (res: Response, payload: Payload): void => {
+  if (!process.env.JWT_SECRET) {
+    throw new Error("JWT_SECRET is not defined in environment variables");
+  }
+
+  const token = jwt.sign({ ...payload }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRES_IN as StringValue || "1d" });
+
+  res.cookie('cement_logistics_token', token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: "strict",
+    maxAge: 24 * 60 * 60 * 1000 // 1 day
+  });
+}
+
+export default generateTokenAndSetCookie;
