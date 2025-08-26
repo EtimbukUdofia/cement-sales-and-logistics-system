02b4e09547c8394bc9424363a47fff61e3e117c3
EtimbukUdofia <etimbukudofia9@gmail.com>
2025-08-26 02:28:28 +0100

    feat(server): implement login functionality

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -8,7 +8,7 @@ export const signup = async (req: Request, res: Response): Promise<void> => {
 
   try {
     if (!username || !email || !password || !role) {
-      res.status(400).json({ message: 'All fields are required' });
+      res.status(400).json({ success: false, message: 'All fields are required' });
       return;
     }
 
@@ -43,7 +43,44 @@ export const signup = async (req: Request, res: Response): Promise<void> => {
   }
 };
 
-export const login = async (): Promise<void> => {
+export const login = async (req: Request, res: Response): Promise<void> => {
+  const { email, password } = req.body;
+
+  try {
+    if (!email || !password) {
+      res.status(400).json({ success: false, message: "Email and password are required" });
+      return;
+    }
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      res.status(400).json({ success: false, message: "Invalid email or password" });
+      return;
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      res.status(400).json({ success: false, message: "Invalid email or password" });
+      return;
+    }
+
+    // generate jwt
+    generateTokenAndSetCookie(res, { userId: user._id, role: user.role });
+
+    res.status(200).json({
+      success: true,
+      message: "Login successful",
+      user: {
+        ...user.toObject(),
+        password: undefined
+      }
+    });
+
+    console.log(`${user.email} logged in`);
+  } catch (error: any) {
+    console.error('Login error:', error.message);
+    res.status(500).json({ success: false, message: 'Server error during login' });
+  }
 
 }
 export const logout = async (): Promise<void> => {
