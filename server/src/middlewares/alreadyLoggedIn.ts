62844765ea307937a361d73027e09d21746f9832
EtimbukUdofia <etimbukudofia9@gmail.com>
2025-08-26 13:16:53 +0100

    feat(server): implement functionality that prevents already logged in users  from loggin in again

---
+++ server/src/middlewares/alreadyLoggedIn.ts
@@ -0,0 +1,22 @@
+import type { NextFunction, Response } from "express";
+import type { AuthRequest } from "../interfaces/interface.ts";
+import jwt from "jsonwebtoken";
+
+const alreadyLoggedIn = (req: AuthRequest, res: Response, next: NextFunction) => {
+  const token = req.cookies.cement_logistics_token;
+
+  if (token) {
+    try {
+      jwt.verify(token, process.env.JWT_SECRET as string);
+      return res.status(403).json({ success: false, message: 'User is already logged in' });
+
+    } catch (error) {
+      next();
+    }
+
+  } else {
+    next();
+  }
+}
+
+export default alreadyLoggedIn;
--- server/src/routes/auth.route.ts
+++ server/src/routes/auth.route.ts
@@ -1,13 +1,14 @@
 import express from "express";
 import { checkAuth, login, logout, signup } from "../controllers/auth.controller.ts";
 import { verifyToken } from "../middlewares/verifyToken.ts";
+import alreadyLoggedIn from "../middlewares/alreadyLoggedIn.ts";
 
 const router = express.Router();
 
 router.get("/check-auth", verifyToken, checkAuth);
 
-router.post("/login", login);
-router.post("/signup", signup);
+router.post("/login", alreadyLoggedIn, login);
+router.post("/signup", alreadyLoggedIn, signup);
 router.post("/logout", logout);
 
 export default router;
