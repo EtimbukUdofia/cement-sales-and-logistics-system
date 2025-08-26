77607403e93f1d821be48fd28a817d296c402e6a
EtimbukUdofia <etimbukudofia9@gmail.com>
2025-08-26 06:58:00 +0100

    fix(server): access the correct cookie property i.e req.cookies.cement_logistics_token instead of req.cookies.token

--- server/src/middlewares/verifyToken.ts
+++ server/src/middlewares/verifyToken.ts
@@ -2,17 +2,18 @@ import type { NextFunction, Response } from "express";
 import type { AuthRequest, DecodedToken } from "../interfaces/interface.ts";
 import jwt from "jsonwebtoken";
 
-export const verifyToken = (req:AuthRequest, res:Response, next:NextFunction) => {
-  const token = req.cookies.token;
+export const verifyToken = async(req:AuthRequest, res:Response, next:NextFunction) => {
+  const token =  req.cookies.cement_logistics_token;
 
   if (!token) {
     return res.status(401).json({success: false, message: 'No token provided' });
   }
 
+
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as DecodedToken;
 
-    if(!decoded || typeof decoded === "string" || !decoded.userId || decoded.role) return res.status(401).json({success:false, message: 'Invalid token' });
+    if(!decoded || typeof decoded === "string" || !decoded.userId || !decoded.role) return res.status(401).json({success:false, message: 'Invalid token' });
 
     req.userId = decoded.userId;
     req.role = decoded.role;
@@ -20,6 +21,6 @@ export const verifyToken = (req:AuthRequest, res:Response, next:NextFunction) =>
     next();
   } catch (err : any) {
     console.error("Error verifying token:", err.message);
-    return res.status(500).json({success:false, message: 'Invalid token' });
+    return res.status(500).json({success:false, message: 'Server Error while trying to validate token' });
   }
 };
