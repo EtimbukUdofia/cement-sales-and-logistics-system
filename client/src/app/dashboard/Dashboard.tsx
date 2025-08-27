0cdc5167aa791bdd09997c3864955456c7d87ce1
EtimbukUdofia <etimbukudofia9@gmail.com>
2025-08-27 18:36:07 +0100

    refactor(dashboard): clean up Dashboard component structure for improved readability

--- client/src/app/dashboard/Dashboard.tsx
+++ client/src/app/dashboard/Dashboard.tsx
@@ -6,16 +6,24 @@ import data from "./data.json"
 
 export default function Dashboard() {
   return (
-        <div className="flex flex-1 flex-col">
-          <div className="@container/main flex flex-1 flex-col gap-2">
-            <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-              <SectionCards />
-              <div className="px-4 lg:px-6">
-                <ChartAreaInteractive />
-              </div>
-              <DataTable data={data} />
-            </div>
+    <div className="flex flex-1 flex-col">
+      <div className="@container/main flex flex-1 flex-col gap-2">
+        <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+
+          {/* Header */}
+          <div className="px-4 lg:px-6">
+            <h2 className="text-2xl font-semibold tracking-tight">Overview</h2>
           </div>
+
+          <SectionCards />
+
+          <div className="px-4 lg:px-6">
+            <ChartAreaInteractive />
+          </div>
+
+          <DataTable data={data} />
         </div>
+      </div>
+    </div>
   )
 }
