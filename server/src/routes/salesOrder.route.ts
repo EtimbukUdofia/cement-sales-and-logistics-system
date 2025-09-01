a56ee73182c330cfdd2089c17d457c280531ade6
EtimbukUdofia <etimbukudofia9@gmail.com>
2025-09-01 03:06:13 +0100

    fix(routes): enforce admin access for sales order management routes

--- server/src/routes/salesOrder.route.ts
+++ server/src/routes/salesOrder.route.ts
@@ -1,13 +1,14 @@
 import express from 'express';
 import { createSalesOrder, deleteSalesOrder, getAllSalesOrders, getSalesOrderById, getSalesOrdersByCustomer, getSalesOrdersByShop, updateSalesOrderStatus } from '../controllers/salesOrder.controller.ts';
+import isAdmin from '../middlewares/isAdmin.ts';
 
 const router = express.Router();
 
-router.get('/', getAllSalesOrders);
+router.get('/', isAdmin, getAllSalesOrders);
 router.get('/:id', getSalesOrderById);
 router.post('/', createSalesOrder);
-router.put('/:id/status', updateSalesOrderStatus);
-router.delete('/:id', deleteSalesOrder);
+router.put('/:id/status', isAdmin, updateSalesOrderStatus);
+router.delete('/:id', isAdmin, deleteSalesOrder);
 router.get('/customer/:customerId', getSalesOrdersByCustomer);
 router.get('/shop/:shopId', getSalesOrdersByShop);
 
