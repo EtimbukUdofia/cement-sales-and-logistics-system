fd9139ba66bb0ad4db1b842066a669dad2fc5b89
EtimbukUdofia <etimbukudofia9@gmail.com>
2025-09-01 05:35:38 +0100

    fix(routes): reorder user route handlers for clarity and consistency

--- server/src/routes/user.route.ts
+++ server/src/routes/user.route.ts
@@ -1,11 +1,12 @@
 import express from "express";
 import isAdmin from "../middlewares/isAdmin.ts";
-import { addUser, deleteUser, getAllUsers, getUserById, updateUser } from "../controllers/user.controller.ts";
+import { addUser, deleteUser, getAllSalesPersons, getAllUsers, getUserById, updateUser } from "../controllers/user.controller.ts";
 
 const router = express.Router();
 
-router.get("/:id", isAdmin, getUserById);
+router.get("/salespersons", getAllSalesPersons);
 router.get("/", isAdmin, getAllUsers);
+router.get("/:id", isAdmin, getUserById);
 router.post("/", isAdmin, addUser);
 router.put("/:id", isAdmin, updateUser);
 router.delete("/:id", isAdmin, deleteUser);
