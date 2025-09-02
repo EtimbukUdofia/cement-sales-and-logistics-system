daf8c4ae03df48cc576b10b7cce21e978e1a1a47
EtimbukUdofia <etimbukudofia9@gmail.com>
2025-09-02 05:13:58 +0100

    fix(auth.route): remove alreadyLoggedIn middleware from login and signup routes

--- server/src/routes/auth.route.ts
+++ server/src/routes/auth.route.ts
@@ -1,14 +1,14 @@
 import express from "express";
 import { checkAuth, login, logout, signup } from "../controllers/auth.controller.ts";
 import { verifyToken } from "../middlewares/verifyToken.ts";
-import alreadyLoggedIn from "../middlewares/alreadyLoggedIn.ts";
+// import alreadyLoggedIn from "../middlewares/alreadyLoggedIn.ts";
 
 const router = express.Router();
 
 router.get("/check-auth", verifyToken, checkAuth);
 
-router.post("/login", alreadyLoggedIn, login);
-router.post("/signup", alreadyLoggedIn, signup);
+router.post("/login", login);
+router.post("/signup", signup);
 router.post("/logout", logout);
 
 export default router;
