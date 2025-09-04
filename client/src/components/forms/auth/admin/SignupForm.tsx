095ad9b276034fdd6f4e641fa4e718d619f8686e
EtimbukUdofia <etimbukudofia9@gmail.com>
2025-09-04 15:26:23 +0100

    fix(LoginForm, SignupForm): update links and comment out GitHub login section

--- client/src/components/forms/auth/admin/SignupForm.tsx
+++ client/src/components/forms/auth/admin/SignupForm.tsx
@@ -127,7 +127,7 @@ export function SignupForm({
         <Button type="submit" className="w-full">
           Sign Up
         </Button>
-        <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
+        {/* <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
           <span className="bg-background text-muted-foreground relative z-10 px-2">
             Or continue with
           </span>
@@ -140,11 +140,11 @@ export function SignupForm({
             />
           </svg>
           Signup with GitHub
-        </Button>
+        </Button> */}
       </div>
       <div className="text-center text-sm">
         Already have an account?{" "}
-        <Link to={"/admin/login"} className="underline underline-offset-4">Log in</Link>
+        <Link to={"/login"} className="underline underline-offset-4">Log in</Link>
       </div>
     </form>
   )
