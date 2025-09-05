478937ace121be57033d853cc41839a89b8427c5
EtimbukUdofia <etimbukudofia9@gmail.com>
2025-09-05 15:47:08 +0100

    refactor: apply verifyToken middleware to shop routes for enhanced security

--- server/src/routes/shop.route.ts
+++ server/src/routes/shop.route.ts
@@ -1,8 +1,11 @@
 import express from 'express';
 import isAdmin from '../middlewares/isAdmin.js';
 import { createShop, deleteShop, getAllShops, getShopById, updateShop } from '../controllers/shop.controller.js';
+import { verifyToken } from '../middlewares/verifyToken.js';
 
 const router = express.Router();
+
+router.use(verifyToken);
 
 router.get('/', getAllShops);
 router.get('/:id', getShopById);
